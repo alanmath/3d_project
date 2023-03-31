b10ba1885649c61cc9597799ac3b2de990660cae
alanmath <alanmatheus187@gmail.com>
2023-03-31 13:16:24 -0300

    sera que eh isso

--- main.py
+++ main.py
@@ -41,12 +41,14 @@ def project_points(points, focal_length):
     transformation_matrix = np.array([[0,0,0,-focal_length], [1,0,0,0], [0,1,0,0], [0,0,-1/focal_length,0]])
     matrix_wp = transformation_matrix @ points
     projected_points = []
-    for point in matrix_wp.T:
+    for i,point in enumerate(matrix_wp.T):
         w = point[3]
-        projected_points.append((point[1]/w, point[2]/w))
+        projected_points.append((point[1]/w, point[2]/w, points[2][i]))
 
 
     return np.array(projected_points)
+
+# Define as coordenadas das paredes e chão do cenário
 
 
 WIDTH, HEIGHT = 800, 600
@@ -86,15 +88,21 @@ pygame.display.set_caption("Cubo Girando")
 clock = pygame.time.Clock()
 
 # Define as constantes de movimentação do jogador
-MOVE_SPEED = 5
+MOVE_SPEED = 3
 ROT_SPEED = 2
 
-# Define a posição inicial e orientação do jogador
-player_pos = np.array([0, 0, 0])
-player_angle = 0
+
 
 running = True
 angle = 0
+
+# Define a posição inicial e orientação do jogador
+player_pos = np.array([0, 0, 0, 1])
+player_angle = 0
+
+player_direction = np.array([0, 0, 1, 1])
+prev_mouse_pos = None
+keys_pressed = {}
 while running:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
@@ -102,22 +110,35 @@ while running:
 
         # Captura as teclas pressionadas pelo usuário
         if event.type == pygame.KEYDOWN:
-            if event.key == pygame.K_w:
-                # Move o jogador para frente
-                focal_length += 10
-            elif event.key == pygame.K_s:
-                # Move o jogador para trás
-                focal_length -= 10
+            keys_pressed[event.key] = True
+        elif event.type == pygame.KEYUP:
+            keys_pressed[event.key] = False
 
-            elif event.key == pygame.K_a:
-                # Rotaciona o jogador para a esquerda
-                player_angle += ROT_SPEED
-            elif event.key == pygame.K_d:
-                # Rotaciona o jogador para a direita
-                player_angle -= ROT_SPEED
+    # Atualiza a posição do jogador de acordo com o estado das teclas
+    if keys_pressed.get(pygame.K_w):
+        player_direction[2] -= MOVE_SPEED
+    elif keys_pressed.get(pygame.K_s):
+        player_direction[2] += MOVE_SPEED
+
+    if keys_pressed.get(pygame.K_a):
+        player_direction = translation_matrix(-ROT_SPEED,0,0) @ player_direction
+    elif keys_pressed.get(pygame.K_d):
+        player_direction = translation_matrix(ROT_SPEED,0,0) @ player_direction
+
+        # Captura os movimentos do mouse
+    if event.type == pygame.MOUSEMOTION:
+        if prev_mouse_pos is not None:
+            # Calcula a diferença entre a posição atual e a posição anterior do mouse
+            mouse_diff = np.array(pygame.mouse.get_pos()) - prev_mouse_pos
+            # Rotaciona o vetor player_direction em torno do eixo y
+            player_direction = rotation_matrix_y(mouse_diff[0]/5) @ player_direction
+            # Rotaciona o vetor player_direction em torno do eixo x
+            player_direction = rotation_matrix_x(-mouse_diff[1]/5) @ player_direction
+        prev_mouse_pos = np.array(pygame.mouse.get_pos())
 
     angle += 0.0
-    rotated_vertices = translation_matrix(player_pos[0], player_pos[1], player_pos[2] + 200) @ rotation_matrix_x(angle) @ rotation_matrix_y(angle + player_angle) @ rotation_matrix_z(angle) @ vertices
+
+    rotated_vertices = translation_matrix(player_direction[0], player_direction[1], player_direction[2] + 200) @ rotation_matrix_x(angle) @ rotation_matrix_y(angle + player_angle) @ rotation_matrix_z(angle) @ vertices
 
     projected_points = project_points(rotated_vertices, focal_length)
     screen.fill((0, 0, 0))
@@ -128,7 +149,15 @@ while running:
     for edge in edges:
         start = (projected_points[edge[0]][0] + WIDTH/2, projected_points[edge[0]][1] + HEIGHT/2)
         end = (projected_points[edge[1]][0] + WIDTH/2, projected_points[edge[1]][1] + HEIGHT/2)
-        pygame.draw.line(screen, (255, 255, 255), start, end, 1)
+
+        start_z_distance = projected_points[edge[0]][2]
+        end_z_distance = projected_points[edge[1]][2]
+
+        grossura_linha = 1/(start_z_distance/1400 + end_z_distance/1400)
+        print(grossura_linha)
+
+        pygame.draw.line(screen, (255, 255, 255), start, end, grossura_linha.astype(int))
+
 
     pygame.display.flip()
     clock.tick(60)
